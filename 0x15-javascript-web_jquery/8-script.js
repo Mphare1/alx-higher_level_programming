33d0034026f14abd4b7853ccc63a4392b32e1267
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-11-06 23:59:01 +0200

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,4 +1,4 @@
-$.get('https://swapi.co/api/films?format=json', function (body) {
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (body) {
   for (let movie of body['results']) {
     $('UL#list_movies').append('<li>' + movie['title'] + '</li>');
   }
