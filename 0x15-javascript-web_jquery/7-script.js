f41b6b8ba49484ae39987f96b65d2be85a9da89d
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-11-06 23:58:19 +0200

    Update 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,3 @@
-$.get('https://swapi.co/api/people/5/?format=json', function (body) {
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (body) {
   $('DIV#character').text(body['name']);
 });
